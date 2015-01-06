de1776d70ecf39c4b503907fd44d4bd37f2581b7
Guo Wang <gw21550@gmail.com>
2015-01-06 15:34:14 -0600

    Added Staff Bio page

--- app/controllers/About.scala
+++ app/controllers/About.scala
@@ -2,6 +2,10 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.Tables._
+import play.api.db.slick._
+import play.api.db.slick.Config.driver.simple._
+
 
 object About extends Controller {
 
@@ -20,6 +24,17 @@ object About extends Controller {
   def write = Action {
     Ok(views.html.about_write())
   }
+  
+  def bio = DBAction { implicit request =>
+      val userList = CrUserBios2005.filter(usr => usr.edition === "2014.2015.2")
+      val execList = userList.filter(usr => usr.lev >= 4.toByte).list
+      val editorList = userList.filter(usr => usr.lev === 3.toByte).list
+      val writerList = userList.filter(usr => usr.lev === 2.toByte).list
+      
+      Ok(views.html.about_bio(execList, editorList, writerList))
+  }
+  
+  def editBio(id: String) = TODO
 
 }
 
---
+++ app/controllers/Bios.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.http._
+import models.Tables._
+import play.api.db.slick._
+import play.api.db.slick.Config.driver.simple._
+
+
+object Bios extends Controller {
+  
+  def staffPhoto(edition: String, uid: Int, version: Int) = DBAction { implicit request =>
+      val photo = StaffPhotos.filter(usr => usr.edition === edition && usr.userId === uid).list.head.data
+      val photoLen = photo.length().toInt
+      //val photoInt = photoLen.toInt
+      val photoBytes : Array[Byte] = photo.getBytes(1, photoLen)
+      
+      Ok(photoBytes).as("image/jpeg")
+  }
+
+}
+
