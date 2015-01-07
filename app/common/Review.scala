f0a09d8e08922c6310cb93750ebefb58cfd1a951
Cody Peter Mello <cody@cs.brown.edu>
2015-01-07 16:45:10 -0500

    Show reviews for different courses.
    Reviews can now be visited at any URL that shows a review on the current
    site. Links to different offerings are presented in a menu to select
    from, and course names in the review itself provide links to the correct
    course.

---
+++ app/common/Review.scala
@@ -0,0 +1,52 @@
+package common
+
+import play.api._
+import play.api.mvc._
+import play.twirl.api.Html
+import scala.xml.Utility.escape
+import models.Tables._
+import play.api.db.slick.Config.driver.simple._
+import play.api.db.slick.Config.driver.simple.{Session => DBSession}
+
+object Review {
+
+  implicit val offeringsOrdering = Ordering.by[(String,List[String]),String](_._1).reverse
+
+  // For a given department and course number, get all offerings.
+  def getOfferings(dept: String, num: String)(implicit session: DBSession) = {
+    CrReview2008.filter(rev =>
+      rev.revision === 0 &&
+      rev.departmentCode === dept &&
+      rev.courseNum === num)
+    .map(rev => (rev.edition, rev.section))
+    .list
+    .groupBy(_._1)
+    .mapValues(_.map(_._2))
+    .toList
+    .sorted
+  }
+
+
+  // Given the textual contents of a review, mark it up with XML
+  def markup_review_content(content: String) = {
+    val ulevel = 0 // TODO: track and store ulevel
+    var xml_content = "";
+    content.replaceAll("\r", "").split("""(\s*\n\s*\t\s*|\s*\n\s*\n\s*)""")
+    .map { para =>
+        if (ulevel >= 6) {
+            // TODO: use a flag to specify if char-count should be included
+            // (instead of always including it for webmasters)
+            xml_content += "<p char-count=\"" + para.length.toString + "\">"
+        } else {
+            xml_content += "<p>";
+        }
+        xml_content += scala.xml.Utility.escape(para).replaceAll("""\b([A-Z]{3,4}) *([0-9]{4})(-?([A-Z]))?(-S([0-9]{1,2}))?\b""", """<a href="/$1/$2$4">$0</a>""") + "</p>"
+    }
+    Html(xml_content);
+  }
+
+  def course_link(dept: String, num: String, offered: String, section: String) = {
+    f"/$dept%s/$num%s/$offered%s/$section%s"
+  }
+
+}
--- app/controllers/Course.scala
+++ app/controllers/Course.scala
@@ -5,6 +5,10 @@ import play.api.mvc._
 import models.Tables._
 import play.api.db.slick._
 import play.api.db.slick.Config.driver.simple._
+import play.api.db.slick.Config.driver.simple.{Session => DBSession}
+import scala.slick.lifted.CanBeQueryCondition
+import common.Codes._
+import common.Review._
 
 object Course extends Controller {
 
@@ -16,9 +20,54 @@ object Course extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def review(dept : String, num : String, offering : String, section : String) = DBAction { implicit request =>
-  val courses = CrReview2008.filter(rev => rev.departmentCode === dept && rev.courseNum === num && rev.edition === offering && rev.section === section).list.toString
-    Ok(play.api.libs.json.Json.toJson(courses))
+  def showReview[T  <: scala.slick.lifted.Column[_]](f: CrReview2008 => T, offerings: List[(String, List[String])])
+    (implicit session: DBSession, wt: CanBeQueryCondition[T]) = {
+    CrReview2008
+    .filter(f)
+    .filter(rev =>
+      rev.revision === 0 &&
+      rev.edition <= Global.current_edition
+    )
+    .sortBy(r =>
+      (r.active.desc, r.insufficient.asc, r.edition.desc, r.numRespondents.desc)
+    )
+    .take(1)
+    .list match {
+      case course::rest => Ok(views.html.course(course, offerings))
+      case Nil => NotFound(<h1>No such course</h1>)
+    }
+  }
+
+  def twoTupleReview(dept: String, num: String, tab: String) = DBAction { implicit request =>
+    val offerings = getOfferings(dept, num)
+    showReview(rev => rev.departmentCode === dept && rev.courseNum === num, offerings)
+  }
+
+  def threeTupleReview(dept: String, num: String, offering: String, tab: String) = DBAction { implicit request =>
+    parse_cis_semester(offering) match {
+      case Some(edition) =>
+        val offerings = getOfferings(dept, num)
+        showReview(rev =>
+          rev.departmentCode === dept &&
+          rev.courseNum === num &&
+          rev.edition === edition, offerings)
+      case None =>
+        NotFound(<h1>Bad semester: {offering}</h1>)
+    }
+  }
+
+  def fourTupleReview(dept : String, num : String, offering : String, section : String, tab: String) = DBAction { implicit request =>
+    parse_cis_semester(offering) match {
+      case Some(edition) =>
+        val offerings = getOfferings(dept, num)
+        showReview(rev =>
+          rev.departmentCode === dept &&
+          rev.courseNum === num &&
+          rev.edition === edition &&
+          rev.section === section, offerings)
+      case None =>
+        NotFound(<h1>Bad semester: {offering}</h1>)
+    }
   }
 
 }
